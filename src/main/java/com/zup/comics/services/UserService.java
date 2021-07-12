242365031e8a9be557b58a9afcbbb68bcbc7f018
Inrikys <inrikys@gmail.com>
2021-07-11 22:17:37 -0300

    feat(Tratamento de exceções)

--- src/main/java/com/zup/comics/services/UserService.java
+++ src/main/java/com/zup/comics/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.zup.comics.entities.User;
 import com.zup.comics.repositories.UserRepository;
+import com.zup.comics.services.exceptions.DatabaseException;
+import com.zup.comics.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -18,12 +21,19 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		
-		return obj.get();
+
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public User insert(User obj) {
+		try {
+			return repository.save(obj);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 }
-
